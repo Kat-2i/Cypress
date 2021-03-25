401a8e5654b95bef05130a6c3af7aa607e9097c8
Katarzyna Kmiotek <katarzyna.kmiotek@2itesting.com>
2021-03-25 12:10:23 +0000

    mistake added to tests

--- cypress/integration/examples/assertions.spec.js
+++ cypress/integration/examples/assertions.spec.js
@@ -166,11 +166,11 @@ context('Assertions', () => {
     })
 
     it('retries the should callback until assertions pass', () => {
-      cy.get('#random-number')
+      cy.get('#random-numberss')
         .should(($div) => {
           const n = parseFloat($div.text())
 
-          expect(n).to.be.gte(2).and.be.lte(10)
+          expect(n).to.be.gte(1).and.be.lte(10)
         })
     })
   })
